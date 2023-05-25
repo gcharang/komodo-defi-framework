9a902e486f2455ebc645b5ba1b3c5810274b19c7
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2023-05-25 19:44:03 +0500

    Fix wasm build

--- mm2src/mm2_rpc/src/lib.rs
+++ mm2src/mm2_rpc/src/lib.rs
@@ -1,4 +1,4 @@
-#[cfg(feature = "rpc_data")] pub mod data;
+pub mod data;
 #[cfg(feature = "rpc_facilities")] pub mod mm_protocol;
 #[cfg(all(feature = "rpc_facilities", target_arch = "wasm32"))]
 pub mod wasm_rpc;
