95b2b3e84ad9144681831d0895561dc4cb04e712
rozhkovdmitrii <rozhkov@komodoplatform.com>
2023-06-09 18:37:29 +0500

    refactor: Introduce include_bytes to read static data from files

--- mm2src/adex_cli/src/tests/mod.rs
+++ mm2src/adex_cli/src/tests/mod.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::time::Duration;
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
@@ -14,7 +13,7 @@ const FAKE_SERVER_WARMUP_TIMEOUT_MS: u64 = 100;
 
 #[tokio::test]
 async fn test_get_version() {
-    tokio::spawn(fake_mm2_server(7784, "src/tests/http_mock_data/version.http"));
+    tokio::spawn(fake_mm2_server(7784, include_bytes!("http_mock_data/version.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -35,7 +34,7 @@ async fn test_get_version() {
 
 #[tokio::test]
 async fn test_get_orderbook() {
-    tokio::spawn(fake_mm2_server(7785, "src/tests/http_mock_data/orderbook.http"));
+    tokio::spawn(fake_mm2_server(7785, include_bytes!("http_mock_data/orderbook.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -53,7 +52,7 @@ async fn test_get_orderbook() {
 
 #[tokio::test]
 async fn test_get_orderbook_with_uuids() {
-    tokio::spawn(fake_mm2_server(7786, "src/tests/http_mock_data/orderbook.http"));
+    tokio::spawn(fake_mm2_server(7786, include_bytes!("http_mock_data/orderbook.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -71,7 +70,7 @@ async fn test_get_orderbook_with_uuids() {
 
 #[tokio::test]
 async fn test_get_orderbook_with_publics() {
-    tokio::spawn(fake_mm2_server(7787, "src/tests/http_mock_data/orderbook.http"));
+    tokio::spawn(fake_mm2_server(7787, include_bytes!("http_mock_data/orderbook.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -89,7 +88,7 @@ async fn test_get_orderbook_with_publics() {
 
 #[tokio::test]
 async fn test_get_enabled() {
-    tokio::spawn(fake_mm2_server(7788, "src/tests/http_mock_data/get_enabled.http"));
+    tokio::spawn(fake_mm2_server(7788, include_bytes!("http_mock_data/get_enabled.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -107,7 +106,7 @@ async fn test_get_enabled() {
 
 #[tokio::test]
 async fn test_get_balance() {
-    tokio::spawn(fake_mm2_server(7789, "src/tests/http_mock_data/balance.http"));
+    tokio::spawn(fake_mm2_server(7789, include_bytes!("http_mock_data/balance.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -125,7 +124,7 @@ async fn test_get_balance() {
 
 #[tokio::test]
 async fn test_enable() {
-    tokio::spawn(fake_mm2_server(7790, "src/tests/http_mock_data/enable.http"));
+    tokio::spawn(fake_mm2_server(7790, include_bytes!("http_mock_data/enable.http")));
     test_activation_scheme().await;
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
@@ -154,7 +153,7 @@ async fn test_activation_scheme() {
 
 #[tokio::test]
 async fn test_buy_morty_for_rick() {
-    tokio::spawn(fake_mm2_server(7791, "src/tests/http_mock_data/buy.http"));
+    tokio::spawn(fake_mm2_server(7791, include_bytes!("http_mock_data/buy.http")));
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_WARMUP_TIMEOUT_MS)).await;
     let mut buffer: Vec<u8> = vec![];
     let response_handler = ResponseHandlerImpl {
@@ -170,23 +169,20 @@ async fn test_buy_morty_for_rick() {
     assert_eq!("Buy order uuid: 4685e133-dfb3-4b31-8d4c-0ffa79933c8e\n", result);
 }
 
-async fn fake_mm2_server(port: u16, response_path: &'static str) {
+async fn fake_mm2_server(port: u16, predefined_response: &'static [u8]) {
     let server = TcpListener::bind(("0.0.0.0", port))
         .await
         .expect("Failed to bind tcp server");
 
     if let Ok((stream, _)) = server.accept().await {
-        tokio::spawn(handle_connection(stream, response_path));
+        tokio::spawn(handle_connection(stream, predefined_response));
     }
 }
 
-async fn handle_connection(mut stream: TcpStream, response_path: &'static str) {
-    let mut file = File::open(response_path).unwrap();
-    let mut buffer: Vec<u8> = vec![];
-    file.read_to_end(&mut buffer).unwrap();
+async fn handle_connection(mut stream: TcpStream, predefined_response: &'static [u8]) {
     let (reader, mut writer) = stream.split();
     reader.readable().await.unwrap();
-    writer.write_all(&buffer).await.unwrap();
+    writer.write_all(predefined_response).await.unwrap();
     tokio::time::sleep(Duration::from_millis(FAKE_SERVER_COOLDOWN_TIMEOUT_MS)).await;
 }
 
