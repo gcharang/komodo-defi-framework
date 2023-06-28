c4d6c71dff4c175621f7d47813727b8d3dec5bee
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2023-06-28 14:24:27 +0500

    fix: make it able to start adex-cli without a config, it was impossible

--- mm2src/adex_cli/src/adex_proc/adex_proc_impl.rs
+++ mm2src/adex_cli/src/adex_proc/adex_proc_impl.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Result};
 use log::{error, info, warn};
 use mm2_rpc::data::legacy::{BalanceResponse, CoinInitResponse, GetEnabledResponse, Mm2RpcResult, MmVersionResponse,
                             OrderbookRequest, OrderbookResponse, SellBuyRequest, SellBuyResponse, Status};
@@ -10,12 +10,26 @@ use super::OrderbookConfig;
 use crate::activation_scheme_db::get_activation_scheme;
 use crate::adex_config::AdexConfig;
 use crate::transport::Transport;
-use crate::{error_bail, warn_bail};
+use crate::{error_anyhow, error_bail, warn_anyhow, warn_bail};
 
 pub(crate) struct AdexProc<'trp, 'hand, 'cfg, T: Transport, H: ResponseHandler, C: AdexConfig + ?Sized> {
-    pub(crate) transport: &'trp T,
+    pub(crate) transport: Option<&'trp T>,
     pub(crate) response_handler: &'hand H,
     pub(crate) config: &'cfg C,
+}
+
+macro_rules! request_legacy {
+    ($request: ident, $response_ty: ty, $self: ident, $handle_method: ident$ (, $opt:expr)*) => {{
+        let transport = $self.transport.ok_or_else(|| warn_anyhow!("Failed to send, transport is not available"))?;
+        match transport.send::<_, $response_ty, Json>($request).await {
+            Ok(Ok(ok)) => $self.response_handler.$handle_method(&ok, $($opt),*),
+            Ok(Err(error)) => $self.response_handler.print_response(error),
+            Err(error) => error_bail!(
+                concat!("Failed to send ", stringify!($response_ty), " request: {}"),
+                error
+            ),
+        }
+    }};
 }
 
 impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_, '_, T, P, C> {
@@ -29,14 +43,10 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
 
         let command = Command::builder()
             .flatten_data(activation_method)
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .build();
 
-        match self.transport.send::<_, CoinInitResponse, Json>(command).await {
-            Ok(Ok(ref ok)) => self.response_handler.on_enable_response(ok),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to enable asset: {asset}, error: {err:?}"),
-        }
+        request_legacy!(command, CoinInitResponse, self, on_enable_response)
     }
 
     pub(crate) async fn get_balance(&self, asset: &str) -> Result<()> {
@@ -44,14 +54,9 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
         let command = Command::builder()
             .method(Method::GetBalance)
             .flatten_data(json!({ "coin": asset }))
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .build();
-
-        match self.transport.send::<_, BalanceResponse, Json>(command).await {
-            Ok(Ok(balance_response)) => self.response_handler.on_balance_response(&balance_response),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to get balance: {err:?}"),
-        }
+        request_legacy!(command, BalanceResponse, self, on_balance_response)
     }
 
     pub(crate) async fn get_enabled(&self) -> Result<()> {
@@ -59,25 +64,16 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
 
         let command = Command::<i32>::builder()
             .method(Method::GetEnabledCoins)
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .build();
-
-        match self
-            .transport
-            .send::<_, Mm2RpcResult<GetEnabledResponse>, Json>(command)
-            .await
-        {
-            Ok(Ok(ok)) => self.response_handler.on_get_enabled_response(&ok),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to get enabled coins: {:?}", err),
-        }
+        request_legacy!(command, Mm2RpcResult<GetEnabledResponse>, self, on_get_enabled_response)
     }
 
     pub(crate) async fn get_orderbook(&self, base: &str, rel: &str, orderbook_config: OrderbookConfig) -> Result<()> {
         info!("Getting orderbook, base: {base}, rel: {rel} ...");
 
         let command = Command::builder()
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .method(Method::GetOrderbook)
             .flatten_data(OrderbookRequest {
                 base: base.to_string(),
@@ -85,13 +81,14 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
             })
             .build();
 
-        match self.transport.send::<_, OrderbookResponse, Json>(command).await {
-            Ok(Ok(ok)) => self
-                .response_handler
-                .on_orderbook_response(ok, self.config, orderbook_config),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to get orderbook: {err:?}"),
-        }
+        request_legacy!(
+            command,
+            OrderbookResponse,
+            self,
+            on_orderbook_response,
+            self.config,
+            orderbook_config
+        )
     }
 
     pub(crate) async fn sell(&self, order: SellBuyRequest) -> Result<()> {
@@ -107,20 +104,11 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
         );
 
         let command = Command::builder()
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .method(Method::Sell)
             .flatten_data(order)
             .build();
-
-        match self
-            .transport
-            .send::<_, Mm2RpcResult<SellBuyResponse>, Json>(command)
-            .await
-        {
-            Ok(Ok(ok)) => self.response_handler.on_sell_response(&ok),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to sell: {err:?}"),
-        }
+        request_legacy!(command, Mm2RpcResult<SellBuyResponse>, self, on_sell_response)
     }
 
     pub(crate) async fn buy(&self, order: SellBuyRequest) -> Result<()> {
@@ -136,47 +124,34 @@ impl<T: Transport, P: ResponseHandler, C: AdexConfig + 'static> AdexProc<'_, '_,
         );
 
         let command = Command::builder()
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .method(Method::Buy)
             .flatten_data(order)
             .build();
-
-        match self
-            .transport
-            .send::<_, Mm2RpcResult<SellBuyResponse>, Json>(command)
-            .await
-        {
-            Ok(Ok(ok)) => self.response_handler.on_buy_response(&ok),
-            Ok(Err(err)) => self.response_handler.print_response(err),
-            Err(err) => error_bail!("Failed to buy: {err:?}"),
-        }
+        request_legacy!(command, Mm2RpcResult<SellBuyResponse>, self, on_buy_response)
     }
 
     pub(crate) async fn send_stop(&self) -> Result<()> {
         info!("Sending stop command");
         let stop_command = Command::<Dummy>::builder()
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .method(Method::Stop)
             .build();
-
-        match self.transport.send::<_, Mm2RpcResult<Status>, Json>(stop_command).await {
-            Ok(Ok(ok)) => self.response_handler.on_stop_response(&ok),
-            Ok(Err(error)) => error_bail!("Failed to stop through the API: {error}"),
-            _ => bail!(""),
-        }
+        request_legacy!(stop_command, Mm2RpcResult<Status>, self, on_stop_response)
     }
 
     pub(crate) async fn get_version(self) -> Result<()> {
         info!("Request for mm2 version");
         let version_command = Command::<Dummy>::builder()
-            .userpass(self.config.rpc_password()?)
+            .userpass(self.get_rpc_password()?)
             .method(Method::Version)
             .build();
+        request_legacy!(version_command, MmVersionResponse, self, on_version_response)
+    }
 
-        match self.transport.send::<_, MmVersionResponse, Json>(version_command).await {
-            Ok(Ok(ok)) => self.response_handler.on_version_response(&ok),
-            Ok(Err(error)) => error_bail!("Failed get version through the API: {error}"),
-            _ => bail!(""),
-        }
+    fn get_rpc_password(&self) -> Result<String> {
+        self.config
+            .rpc_password()
+            .ok_or_else(|| error_anyhow!("Failed to get rpc_password, not set"))
     }
 }
