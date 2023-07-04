befa9be8989dab9ed07c196548907f9cccae9985
rozhkovdmitrii <rozhkov@komodoplatform.com>
2023-07-04 16:42:04 +0500

    refactor: orderbook_depth.rs - spit and polish

--- mm2src/adex_cli/src/adex_proc/response_handler/orderbook_depth.rs
+++ mm2src/adex_cli/src/adex_proc/response_handler/orderbook_depth.rs
@@ -13,23 +13,31 @@ pub(super) fn on_orderbook_depth(
     response: Mm2RpcResult<Vec<PairWithDepth>>,
 ) -> anyhow::Result<()> {
     let mut term_table = term_table_blank(TableStyle::empty(), false, false, false);
-    term_table.add_row(Row::new(vec![
-        TableCell::new(""),
-        TableCell::new_with_alignment_and_padding("Bids", 1, Alignment::Left, false),
-        TableCell::new_with_alignment_and_padding("Asks", 1, Alignment::Left, false),
-    ]));
+    term_table.add_row(orderbook_depth_header_row());
     for data in response.result {
-        term_table.add_row(Row::new(vec![
-            TableCell::new_with_alignment_and_padding(
-                format!("{}/{}:", data.pair.0, data.pair.1),
-                1,
-                Alignment::Right,
-                false,
-            ),
-            TableCell::new_with_alignment_and_padding(data.depth.bids, 1, Alignment::Left, false),
-            TableCell::new_with_alignment_and_padding(data.depth.asks, 1, Alignment::Left, false),
-        ]))
+        term_table.add_row(orderbook_depth_row(data))
     }
     write_safe_io!(writer, "{}", term_table.render().replace('\0', ""));
     Ok(())
+}
+
+fn orderbook_depth_header_row() -> Row<'static> {
+    Row::new(vec![
+        TableCell::new(""),
+        TableCell::new_with_alignment_and_padding("Bids", 1, Alignment::Left, false),
+        TableCell::new_with_alignment_and_padding("Asks", 1, Alignment::Left, false),
+    ])
+}
+
+fn orderbook_depth_row(data: PairWithDepth) -> Row<'static> {
+    Row::new(vec![
+        TableCell::new_with_alignment_and_padding(
+            format!("{}/{}:", data.pair.0, data.pair.1),
+            1,
+            Alignment::Right,
+            false,
+        ),
+        TableCell::new_with_alignment_and_padding(data.depth.bids, 1, Alignment::Left, false),
+        TableCell::new_with_alignment_and_padding(data.depth.asks, 1, Alignment::Left, false),
+    ])
 }
