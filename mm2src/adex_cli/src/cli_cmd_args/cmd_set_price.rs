8c44abf07ad5b77333147f34030f155a187a9af9
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2023-07-05 12:31:11 +0500

    refactor: polish set_price command

--- mm2src/adex_cli/src/cli_cmd_args/cmd_set_price.rs
+++ mm2src/adex_cli/src/cli_cmd_args/cmd_set_price.rs
@@ -1,52 +1,67 @@
 use clap::Args;
+use std::mem::take;
+
 use mm2_number::MmNumber;
 use mm2_rpc::data::legacy::SetPriceReq;
-use std::mem::take;
 
 use super::parse_mm_number;
 
 #[derive(Args)]
-#[command(about = "Place an order on the orderbook, and if relies on this node acting as a maker")]
+#[command(about = "Place an order on the orderbook. The setprice order is always considered a sell")]
 pub(crate) struct SetPriceArgs {
     #[arg(help = "The name of the coin the user desires to sell")]
     base: String,
     #[arg(help = "The name of the coin the user desires to receive")]
     rel: String,
-    #[arg(help = "The price in rel the user is willing to receive per one unit of the base coin", value_parser = parse_mm_number)]
+    #[arg(
+        value_parser = parse_mm_number,
+        help = "The price in rel the user is willing to receive per one unit of the base coin"
+    )]
     price: MmNumber,
     #[command(flatten)]
     delegate: SetPriceVolumeGroup,
     #[arg(
         long,
+        short,
+        value_parser = parse_mm_number,
         help = "The minimum amount of base coin available for the order; it must be less or equal than volume param; \
                 the following values must be greater than or equal to the min_trading_vol of the corresponding coin",
-        value_parser = parse_mm_number
     )]
     min_volume: Option<MmNumber>,
-    #[arg(long, help = "Cancel all existing orders for the selected pair")]
+    #[arg(
+        long,
+        short,
+        visible_alias = "cancel",
+        help = "Cancel all existing orders for the selected pair"
+    )]
     cancel_prev: bool,
     #[arg(
         long,
+        visible_alias = "bc",
         help = "Number of required blockchain confirmations for base coin atomic swap transaction"
     )]
     base_confs: Option<u64>,
     #[arg(
         long,
+        visible_alias = "bn",
         help = "Whether dPoW notarization is required for base coin atomic swap transaction"
     )]
     base_nota: Option<bool>,
     #[arg(
         long,
+        visible_alias = "rc",
         help = "Number of required blockchain confirmations for rel coin atomic swap transaction"
     )]
     rel_confs: Option<u64>,
     #[arg(
         long,
+        visible_alias = "rn",
         help = "Whether dPoW notarization is required for rel coin atomic swap transaction"
     )]
     rel_nota: Option<bool>,
     #[arg(
         long,
+        short,
         visible_alias = "save",
         help = "If true, each order's short record history is stored in a local SQLite database table, \
                 and when the order is cancelled or fully matched, it's history will be saved as a json file",
@@ -84,6 +99,7 @@ struct SetPriceVolumeGroup {
     #[arg(
         group = "set-price-volume",
         long,
+        short = 'M',
         help = "Use the entire coin balance for the order, taking 0.001 coins into reserve to account for fees",
         default_value_t = false
     )]
@@ -91,6 +107,7 @@ struct SetPriceVolumeGroup {
     #[arg(
         group = "set-price-volume",
         long,
+        short,
         help = "The maximum amount of base coin available for the order, ignored if max is true; the following values must be greater than or equal to the min_trading_vol of the corresponding coin",
         value_parser = parse_mm_number
     )]
