1d8df3c83467ada8fc10c3d0337875429d4edebc
dimxy <dimxy@komodoplatform.com>
2023-11-15 21:16:07 +0500

    dispose extra chain_id replay protection, fix trimming zeros for trezor eth sign request

--- mm2src/trezor/src/eth/eth_command.rs
+++ mm2src/trezor/src/eth/eth_command.rs
@@ -2,7 +2,7 @@ use crate::proto::messages_ethereum as proto_ethereum;
 use crate::result_handler::ResultHandler;
 use crate::{serialize_derivation_path, OperationFailure, TrezorError, TrezorResponse, TrezorResult, TrezorSession};
 use bip32::DerivationPath;
-use ethcore_transaction::{Action, Transaction as UnSignedEthTx, UnverifiedTransaction as UnverifiedEthTx};
+use ethcore_transaction::{signature, Action, Transaction as UnSignedEthTx, UnverifiedTransaction as UnverifiedEthTx};
 use ethkey::Signature;
 use hw_common::primitives::XPub;
 use mm2_err_handle::prelude::MmError;
@@ -16,7 +16,6 @@ impl<'a> TrezorSession<'a> {
             encoded_network: None,
             chunkify: None,
         };
-        //let mut tx_request = self.send_get_eth_address_req(req).await?.ack_all().await?;
         let result_handler = ResultHandler::<proto_ethereum::EthereumAddress>::new(Ok);
         let result = self.call(req, result_handler).await?.ack_all().await?;
         Ok(result.address)
@@ -47,20 +46,16 @@ impl<'a> TrezorSession<'a> {
 
         while let Some(data_length) = tx_request.data_length {
             if data_length > 0 {
-                println!("data_length={}", data_length);
                 let req = proto_ethereum::EthereumTxAck {
                     data_chunk: data.splice(..std::cmp::min(1024, data.len()), []).collect(),
                 };
-                //ack.set_data_chunk(data.splice(..std::cmp::min(1024, data.len()), []).collect());
-
-                //resp = self.call(ack, Box::new(|_, m: proto_ethereum::EthereumTxRequest| Ok(m)))?.ok()?;
                 tx_request = self.send_eth_tx_ack(req).await?.ack_all().await?;
             } else {
                 break;
             }
         }
 
-        let sig = extract_eth_signature(&tx_request, chain_id)?;
+        let sig = extract_eth_signature(&tx_request)?;
         Ok(unsigned_tx.clone().with_signature(sig, Some(chain_id)))
     }
 
@@ -82,11 +77,13 @@ impl<'a> TrezorSession<'a> {
 }
 
 /// TODO: maybe there is a more standard way
-fn my_trim_u8(arr: &[u8]) -> Vec<u8> {
+fn left_trim_u8(arr: &[u8]) -> Vec<u8> {
     let mut z = 0;
     for i in arr {
         if i == &0 {
             z += 1;
+        } else {
+            break;
         }
     }
     arr[z..].to_vec()
@@ -116,11 +113,11 @@ fn to_sign_eth_message(
     };
     proto_ethereum::EthereumSignTx {
         address_n: serialize_derivation_path(derivation_path), // derivation_path.iter().map(|child| child.0).collect(),
-        nonce: Some(my_trim_u8(&nonce)),
-        gas_price: my_trim_u8(&gas_price),
-        gas_limit: my_trim_u8(&gas_limit),
+        nonce: Some(left_trim_u8(&nonce)),
+        gas_price: left_trim_u8(&gas_price),
+        gas_limit: left_trim_u8(&gas_limit),
         to: addr_hex,
-        value: Some(my_trim_u8(&value)),
+        value: Some(left_trim_u8(&value)),
         data_initial_chunk: Some(data.splice(..std::cmp::min(1024, data.len()), []).collect()),
         data_length: if data.is_empty() { None } else { Some(data.len() as u32) },
         chain_id,
@@ -130,18 +127,17 @@ fn to_sign_eth_message(
     }
 }
 
-fn extract_eth_signature(tx_request: &proto_ethereum::EthereumTxRequest, chain_id: u64) -> TrezorResult<Signature> {
+fn extract_eth_signature(tx_request: &proto_ethereum::EthereumTxRequest) -> TrezorResult<Signature> {
     match (
         tx_request.signature_r.as_ref(),
         tx_request.signature_s.as_ref(),
         tx_request.signature_v,
     ) {
         (Some(r), Some(s), Some(v)) => {
-            let v_fixed = if v <= 1 { v + 2 * (chain_id as u32) + 35 } else { v };
             Ok(Signature::from_rsv(
                 &H256::from_slice(r.as_slice()),
                 &H256::from_slice(s.as_slice()),
-                v_fixed as u8,
+                signature::check_replay_protection(v as u64), // remove replay protection added by trezor as the ethcore lib will add it itself
             ))
         },
         (_, _, _) => Err(MmError::new(TrezorError::Failure(OperationFailure::InvalidSignature))),
